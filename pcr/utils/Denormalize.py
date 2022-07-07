added point cloud assets and fixed normalization code

pcr/utils/Denormalize.py
@@ -6,5 +6,5 @@ class Denormalize:
         self.scale = config.scale
 
     def __call__(self, pointcloud, context):
-        pointcloud = (pointcloud + torch.tensor(context['offset'], device=pointcloud.device)) / self.scale * torch.tensor(context['diameter'], device=pointcloud.device) + torch.tensor(context['center'], device=pointcloud.device)
+        pointcloud = (pointcloud + context['offset']) / self.scale * context['diameter'] + context['center']
         return pointcloud

tests/test_pcr_grasping.py
@@ -1,8 +1,11 @@
 import numpy as np
-from open3d.cpu.pybind.geometry import PointCloud, TriangleMesh
-from open3d.cpu.pybind.utility import Vector3dVector, Vector3iVector
-from open3d.visualization import draw
-from scipy.spatial.transform import Rotation
+try:
+    from open3d.cpu.pybind.geometry import PointCloud
+    from open3d.cpu.pybind.utility import Vector3dVector
+    from open3d.visualization import draw
+    visualize = True
+except ImportError:
+    visualize = False
 
 from pcr import __version__
 from pcr.model import PCRNetwork as Model
@@ -15,9 +18,8 @@ import torch
 def test_version():
     assert __version__ == '0.1.0'
 
-    id = '1m5301hl'
-    version = 'v53'
-    ckpt_path = download_checkpoint(f'model-{id}-{version}.ckpt')
+    ckpt_path = download_checkpoint(f'grasping.ckpt')
+    asset_path = download_asset(f'partial_bleach_317.npy')
 
     model = Model.load_from_checkpoint(ckpt_path, config=Config.Model)
     model.cuda()
@@ -25,101 +27,25 @@ def test_version():
 
     partial = np.load('../pcr/data/MCD/training_data/grasp_database/avocado_poisson_000/pointclouds/_0_0_0_partial.npy')
 
-    normalized_partial, ctx = Normalize(Config.Processing)(partial)
+    partial, ctx = Normalize(Config.Processing)(partial)
 
-    draw([PointCloud(points=Vector3dVector(normalized_partial)).paint_uniform_color([1, 0, 0]),
-          PointCloud(points=Vector3dVector(partial)).paint_uniform_color([0, 0, 1]),
-          create_cube()])
+    partial = torch.tensor(partial, dtype=torch.float32).cuda().unsqueeze(0)
+    complete, probabilities = model(partial)
 
-    tensor_partial = torch.tensor(normalized_partial, dtype=torch.float32, device=Config.General.device).unsqueeze(0)
+    complete = complete.squeeze(0).cpu().numpy()
+    partial = partial.squeeze(0).cpu().numpy()
 
-    normalized_complete, probabilities = model(tensor_partial)
-    complete = Denormalize(Config.Processing)(normalized_complete, ctx)
+    complete = Denormalize(Config.Processing)(complete, ctx)
+    partial = Denormalize(Config.Processing)(partial, ctx)
 
-    draw([PointCloud(points=Vector3dVector(normalized_partial)).paint_uniform_color([1, 0, 0]),
-          PointCloud(points=Vector3dVector(partial)).paint_uniform_color([0, 0, 1]),
-          PointCloud(points=Vector3dVector(normalized_complete[0].cpu().numpy())).paint_uniform_color([1, 1, 0]),
-          PointCloud(points=Vector3dVector(complete[0].cpu().numpy())).paint_uniform_color([0, 1, 1]),
-          create_cube()
-          ])
+    if visualize:
+        draw([
+              PointCloud(points=Vector3dVector(partial)).paint_uniform_color([0, 0, 1]),
+              PointCloud(points=Vector3dVector(complete)).paint_uniform_color([0, 1, 1]),
+              ])
 
     print(complete)
 
-def create_cube():
-    cube = []
-    for _ in range(2500):
-        p = np.random.rand((3)) - 0.5
-        cube.append([-0.5, p[1], p[2]])
-        cube.append([0.5, p[1], p[2]])
-        cube.append([p[0], -0.5, p[2]])
-        cube.append([p[0], 0.5, p[2]])
-        cube.append([p[0], p[1], -0.5])
-        cube.append([p[0], p[1], 0.5])
-
-    cb = PointCloud()
-    cb.points = Vector3dVector(np.array(cube))
-    cb.paint_uniform_color([0, 1, 0])
-
-    return cb
-
-def correct_pose(pose):
-    pre = Rotation.from_euler('yz', [90, 180], degrees=True).as_matrix()
-    post = Rotation.from_euler('zyz', [180, 180, 90], degrees=True).as_matrix()
-
-    t = np.eye(4)
-    t[:3, :3] = np.dot(post, np.dot(pose[:3, :3].T, pre))
-
-    return t
-
-
-def get_x_rot(pose):
-    t = correct_pose(pose)
-
-    x, y, z = Rotation.from_matrix(t[:3, :3].T).as_euler('xyz', degrees=True)
-
-    rot_x = np.eye(4)
-    rot_x[:3, :3] = Rotation.from_euler('x', x, degrees=True).as_matrix() @ Rotation.from_euler('x', 180,
-                                                                                                degrees=True).as_matrix()
-
-    return rot_x
-
-
-def get_y_rot(pose):
-    t = correct_pose(pose)
-
-    x, y, z = Rotation.from_matrix(t[:3, :3].T).as_euler('xyz', degrees=True)
-    if z < 0:
-        t[:3, :3] = Rotation.from_euler('zxy', [-z, -x, y], degrees=True).as_matrix()
-    else:
-        t[:3, :3] = Rotation.from_euler('zxy', [-z, x, -y], degrees=True).as_matrix()
-
-    theta_y = Rotation.from_matrix(t[:3, :3]).as_euler('zxy', degrees=True)[2]
-    rot_y = np.eye(4)
-    rot_y[:3, :3] = Rotation.from_euler('y', theta_y, degrees=True).as_matrix()
-
-    return rot_y
-
-
-def match_mesh_to_partial(vertices, pose):
-    y1_pose, x2_pose, y3_pose = pose
-
-    y_rot = get_y_rot(y1_pose)
-    x2_rot = get_x_rot(x2_pose)
-    y3_rot = get_y_rot(y3_pose)
-
-    base_rot = np.eye(4)
-    base_rot[:3, :3] = Rotation.from_euler('xyz', [180, 0, -90], degrees=True).as_matrix()
-
-    t = y_rot @ x2_rot @ y3_rot @ base_rot
-
-    pc = np.ones((np.size(vertices, 0), 4))
-    pc[:, 0:3] = vertices
-
-    pc = pc.T
-    pc = t @ pc
-    pc = pc.T[..., :3]
-
-    return pc
 
 if __name__ == '__main__':
     test_version()